8259d8ab79fd77a114c47ad1a5d8649831808780
ByeongukYun <byeonguk1228@gmail.com>
2025-01-03 12:44:10 +0900

    MovieController created

---
+++ src/main/java/com/example/moviecommu/Controller/MovieController.java
@@ -0,0 +1,13 @@
+package com.example.moviecommu.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+    public String searchMovie() {
+
+    }
+    public String showMovieRank() {
+
+    }
+}
