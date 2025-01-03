ddbff45a5b1867cf63d1baed3498def7fb20fa34
ByeongukYun <byeonguk1228@gmail.com>
2025-01-03 10:50:45 +0900

    first commit

---
+++ src/main/java/com/example/moviecommu/MovieCommuApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviecommu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCommuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCommuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moviecommu/MovieCommuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviecommu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCommuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
