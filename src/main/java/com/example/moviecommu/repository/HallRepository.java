db19bcd8339d0cfa343c41e76b6b0ce59efa2fcf
py0o0 <pantom0425@naver.com>
2025-01-13 17:36:35 +0900

    Delete src directory

--- src/main/java/com/example/moviecommu/repository/HallRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.moviecommu.repository;
-
-import com.example.moviecommu.entity.Hall;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface HallRepository {
-    Hall findByHallId(long hallId);
-}
